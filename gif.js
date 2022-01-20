01d4f24a100c86aacc3495e423b324c58130536a
satyamsharma17 <satyamsharma1725@gmail.com>
2022-01-20 20:23:37 +0530

    first commit

---
+++ gif.js
@@ -0,0 +1,41 @@
+const GIPHY_KEY = 'G37gLaEizhTkiBREF1bLvhKf1kKCAPhC';
+
+(function () {
+    function giphySearch(keyword) {
+        return fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${GIPHY_KEY}&limit=5`)
+        .then(response => response.json());
+    }
+  
+    function appendImage(img) {
+      let $div = $('<div class="img-wrapper"></div>');
+      $('<div class="inner"></div>').append(img).appendTo($div);
+      $('#thumbs').append($div)
+    }
+  
+    function showLoader() {
+      $('.loader-wrapper').addClass('shown');
+    }
+  
+    function hideLoader() {
+      $('.loader-wrapper').removeClass('shown');
+    }
+  
+    (function listenOnFormSubmit() {
+      $('#searchForm').submit((ev) => {
+        ev.preventDefault();
+        let $input = $('#searchInput');
+  
+        main($input.val());
+      });
+    })();
+  
+    async function main(searchKeyword) {
+      const result = await giphySearch(searchKeyword);
+      $('#thumbs').html('');
+      result.data.forEach(gif => {
+        let img = new Image();
+        img.src = gif.images.original.url;
+        appendImage(img);
+      })
+    }
+  })();
